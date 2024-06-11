bcef6e640410d4aebbd20d4147f2e84fb20f289a
yesman5307 <yesman5307@gmail.com>
2024-06-11 15:44:41 +0900

    constants --> final_variables로 파일명 변경

---
+++ variables/late_variables.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  late final String name; // late var name; // late String name; // late final String name; // late final name
+  // do something, go to api
+   name = 'nico';
+  print(name);
+ 
+
+
+}
